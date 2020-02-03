7f155faad5e93bcdfa83434291f607cdaa7107e6
Drew FINNIS <dfinnis@e1r1p10.42.fr>
2020-02-03 16:07:19 +0100

    feature/Grace correct, shortened Colleen

--- C/Colleen.c
+++ C/Colleen.c
@@ -3,14 +3,14 @@
    comment outside of program
 */
 void	print(char *c){
-	printf(c, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 34, c, 34, 10, 10, 10, 10);
+	printf(c, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 34, c, 34, 10, 10, 10);
 }
 
 int	main() {
 	/*
 	   comment in main
 	*/
-	char *c="# include <stdio.h>%c/*%c   comment outside of program%c*/%cvoid	print(char *c){%c	printf(c, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 34, c, 34, 10, 10, 10, 10);%c}%c%cint	main() {%c	/*%c	   comment in main%c	*/%c	char *c=%c%s%c;%c	print(c);%c	return (0);%c}%c";
+	char *c="# include <stdio.h>%c/*%c   comment outside of program%c*/%cvoid	print(char *c){%c	printf(c, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 34, c, 34, 10, 10, 10);%c}%c%cint	main() {%c	/*%c	   comment in main%c	*/%c	char *c=%c%s%c;%c	print(c);%c	return (0);%c}";
 	print(c);
 	return (0);
 }
--- C/Grace.c
+++ C/Grace.c
@@ -1,33 +1,9 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   Grace.c                                            :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: dfinnis <marvin@42.fr>                     +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/01/26 14:03:00 by dfinnis           #+#    #+#             */
-/*   Updated: 2020/01/26 18:59:57 by dfinnis          ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
-# include <unistd.h>
-
+#include <stdio.h>
+#include <unistd.h>
 /*
    comment
 */
-
-// To_do: 
-// No main declared
-// Three defines only
-
-// #include
-// # define FT(x)int main(){ /* code */ }
-
-// FT(xxxxxxxx)
-
-int main() {
-	write(1, "Grace\n", 6);////
-	// create Grace_kid.c
-	write(1, "bye Grace\n", 10);//////
-	return (0);
-}
+#define CHAR		"#include <stdio.h>%c#include <unistd.h>%c/*%c   comment%c*/%c#define CHAR%c%c%c%s%c%c#define PRINT(x)%cfprintf(x,CHAR,10,10,10,10,10,9,9,34,CHAR,34,10,9,10,9,9,9,34,34,34,34,10)%c#define FT %c%c%cint main() { FILE *f; f=fopen(%cGrace_kid.c%c,%cw%c); PRINT(f); return 0; }%cFT"
+#define PRINT(x)	fprintf(x,CHAR,10,10,10,10,10,9,9,34,CHAR,34,10,9,10,9,9,9,34,34,34,34,10)
+#define FT 			int main() { FILE *f; f=fopen("Grace_kid.c","w"); PRINT(f); return 0; }
+FT
