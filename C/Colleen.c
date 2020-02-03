d0127f27f58daaae60b28e82760f7d6931857fdf
Drew FINNIS <dfinnis@e1r1p10.42.fr>
2020-02-03 15:26:49 +0100

    fix/comments different as per instruction, output same as file contents

--- C/Colleen.c
+++ C/Colleen.c
@@ -1,18 +1,6 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   Colleen.c                                          :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: dfinnis <marvin@42.fr>                     +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/01/26 14:01:14 by dfinnis           #+#    #+#             */
-/*   Updated: 2020/01/26 14:01:16 by dfinnis          ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 # include <stdio.h>
 /*
-   comment
+   comment outside of program
 */
 void	print(char *c){
 	printf(c, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 34, c, 34, 10, 10, 10, 10);
@@ -20,9 +8,9 @@ void	print(char *c){
 
 int	main() {
 	/*
-	   comment
+	   comment in main
 	*/
-	char *c="# include <stdio.h>%c/*%c   comment%c*/%cvoid	print(char *c){%c	printf(c, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 34, c, 34, 10, 10, 10, 10);%c}%c%cint	main() {%c	/*%c	   comment%c	*/%c	char *c=%c%s%c%c	print(c);%c	return (0);%c}%c";
+	char *c="# include <stdio.h>%c/*%c   comment outside of program%c*/%cvoid	print(char *c){%c	printf(c, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 34, c, 34, 10, 10, 10, 10);%c}%c%cint	main() {%c	/*%c	   comment in main%c	*/%c	char *c=%c%s%c;%c	print(c);%c	return (0);%c}%c";
 	print(c);
 	return (0);
 }
