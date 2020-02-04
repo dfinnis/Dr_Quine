8dc43d69686e6c167797ba5ce4823b9d60d55150
Drew FINNIS <dfinnis@e1r1p1.42.fr>
2020-02-04 11:34:27 +0100

    refacto/Collen printf args shorter format

--- C/Colleen.c
+++ C/Colleen.c
@@ -3,14 +3,14 @@
    comment outside of program
 */
 void	print(char *s){
-	printf(s,10,10,10,10,10,10,10,10,10,10,10,10,34,s,34,10,10,10);
+	printf(s,10,34,s);
 }
 
 int	main() {
 	/*
 	   comment in main
 	*/
-	char *s="# include <stdio.h>%c/*%c   comment outside of program%c*/%cvoid	print(char *s){%c	printf(s,10,10,10,10,10,10,10,10,10,10,10,10,34,s,34,10,10,10);%c}%c%cint	main() {%c	/*%c	   comment in main%c	*/%c	char *s=%c%s%c;%c	print(s);%c	return (0);%c}";
+	char *s="# include <stdio.h>%1$c/*%1$c   comment outside of program%1$c*/%1$cvoid	print(char *s){%1$c	printf(s,10,34,s);%1$c}%1$c%1$cint	main() {%1$c	/*%1$c	   comment in main%1$c	*/%1$c	char *s=%2$c%3$s%2$c;%1$c	print(s);%1$c	return (0);%1$c}";
 	print(s);
 	return (0);
 }
