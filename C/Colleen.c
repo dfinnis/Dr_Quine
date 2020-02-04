30f393acf9a8c639b998a971b6110ec3de67b54d
Drew FINNIS <dfinnis@e1r1p1.42.fr>
2020-02-04 07:27:25 +0100

    refacto/string name c to s

--- C/Colleen.c
+++ C/Colleen.c
@@ -2,15 +2,15 @@
 /*
    comment outside of program
 */
-void	print(char *c){
-	printf(c,10,10,10,10,10,10,10,10,10,10,10,10,34,c,34,10,10,10);
+void	print(char *s){
+	printf(s,10,10,10,10,10,10,10,10,10,10,10,10,34,s,34,10,10,10);
 }
 
 int	main() {
 	/*
 	   comment in main
 	*/
-	char *c="# include <stdio.h>%c/*%c   comment outside of program%c*/%cvoid	print(char *c){%c	printf(c,10,10,10,10,10,10,10,10,10,10,10,10,34,c,34,10,10,10);%c}%c%cint	main() {%c	/*%c	   comment in main%c	*/%c	char *c=%c%s%c;%c	print(c);%c	return (0);%c}";
-	print(c);
+	char *s="# include <stdio.h>%c/*%c   comment outside of program%c*/%cvoid	print(char *s){%c	printf(s,10,10,10,10,10,10,10,10,10,10,10,10,34,s,34,10,10,10);%c}%c%cint	main() {%c	/*%c	   comment in main%c	*/%c	char *s=%c%s%c;%c	print(s);%c	return (0);%c}";
+	print(s);
 	return (0);
 }
