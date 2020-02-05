c1de31dd9d8e1ca9842b86eaa22ab82abdf9e654
dfinnis <dfinnis@student.42.fr>
2020-02-05 12:49:24 +0100

    refacto/char variable name s to self

--- C/Colleen.c
+++ C/Colleen.c
@@ -2,15 +2,15 @@
 /*
    comment outside of program
 */
-void	print(char *s){
-	printf(s,10,34,s);
+void	print(char *self){
+	printf(self,10,34,self);
 }
 
 int	main() {
 	/*
 	   comment in main
 	*/
-	char *s="# include <stdio.h>%1$c/*%1$c   comment outside of program%1$c*/%1$cvoid	print(char *s){%1$c	printf(s,10,34,s);%1$c}%1$c%1$cint	main() {%1$c	/*%1$c	   comment in main%1$c	*/%1$c	char *s=%2$c%3$s%2$c;%1$c	print(s);%1$c	return (0);%1$c}";
-	print(s);
+	char *self="# include <stdio.h>%1$c/*%1$c   comment outside of program%1$c*/%1$cvoid	print(char *self){%1$c	printf(self,10,34,self);%1$c}%1$c%1$cint	main() {%1$c	/*%1$c	   comment in main%1$c	*/%1$c	char *self=%2$c%3$s%2$c;%1$c	print(self);%1$c	return (0);%1$c}";
+	print(self);
 	return (0);
 }
