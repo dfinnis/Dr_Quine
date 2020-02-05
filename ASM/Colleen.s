33311d7e4c55e8b44207858a0410fd62b0faff3e
dfinnis <dfinnis@student.42.fr>
2020-02-05 19:45:14 +0100

    feature/colleen first draft, untested

--- ASM/Colleen.s
+++ ASM/Colleen.s
@@ -1,6 +1,20 @@
+;whoami?
+section .data
+self: db "self string"
+
+section .text
+global start
 global _main
+extern _printf
+
 _main:
-leave
-call end
-end:
-ret
+	push rpb
+	mov rbp, rsp
+	;I must express myself
+	lea rdi, [rel fmt]
+	call _printf
+	call _return
+
+_return:
+	leave
+	ret
