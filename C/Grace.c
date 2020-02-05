b3b792971073a4600d7ae8d618340f01a18b811f
dfinnis <dfinnis@student.42.fr>
2020-02-05 16:46:36 +0100

    refacto/comment child to kid

--- C/Grace.c
+++ C/Grace.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <unistd.h>
 /*
-   I must define and clone myself, spawning my child
+   I must define and clone myself, spawning my kid
 */
-#define SELF		"#include <stdio.h>%1$c#include <unistd.h>%1$c/*%1$c   I must define and clone myself, spawning my child%1$c*/%1$c#define SELF		%2$c%3$s%2$c%1$c#define PRINT(f)	fprintf(f,SELF,10,34,SELF)%1$c#define FT 			int main() { FILE *f; f=fopen(%2$cGrace_kid.c%2$c,%2$cw%2$c); PRINT(f); return 0; }%1$cFT"
+#define SELF		"#include <stdio.h>%1$c#include <unistd.h>%1$c/*%1$c   I must define and clone myself, spawning my kid%1$c*/%1$c#define SELF		%2$c%3$s%2$c%1$c#define PRINT(f)	fprintf(f,SELF,10,34,SELF)%1$c#define FT 			int main() { FILE *f; f=fopen(%2$cGrace_kid.c%2$c,%2$cw%2$c); PRINT(f); return 0; }%1$cFT"
 #define PRINT(f)	fprintf(f,SELF,10,34,SELF)
 #define FT 			int main() { FILE *f; f=fopen("Grace_kid.c","w"); PRINT(f); return 0; }
 FT
