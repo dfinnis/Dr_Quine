3931d98adf3c370ff87574559dc838b88ffe2c90
Anya Schukin <anya.schukin@gmail.com>
2020-02-06 15:46:25 +0100

    feature/Grace correct

--- ASM/Grace.s
+++ ASM/Grace.s
@@ -1,5 +1,5 @@
 ; I must clone myself, spawning my kid
-%define SELF "oh hi%1$cnewline%1$c%2$cquotes%2$c%3$s"
+%define SELF "; I must clone myself, spawning my kid%1$c%3$cdefine SELF %2$c%4$s%2$c%1$c%3$cdefine DECLARE_MAIN global _main%1$c%3$cdefine MAIN _main:%1$csection .data%1$cself db SELF, 0%1$ckidsname db %2$cGrace_kid.s%2$c, 0%1$csection .text%1$cextern _dprintf%1$cDECLARE_MAIN%1$cMAIN%1$cpush rbp%1$cmov rbp, rsp%1$clea rdi, [rel kidsname]%1$cmov rsi, 0x0202%1$cmov rax, 0x2000005%1$csyscall%1$cmov rdi, rax%1$clea rsi, [rel self]%1$cmov rdx, 10%1$cmov rcx, 34%1$cmov r8, 37%1$clea r9, [rel self]%1$ccall _dprintf%1$cleave%1$cret"
 %define DECLARE_MAIN global _main
 %define MAIN _main:
 section .data
@@ -19,7 +19,8 @@ mov rdi, rax
 lea rsi, [rel self]
 mov rdx, 10
 mov rcx, 34
-lea r8, [rel self]
+mov r8, 37
+lea r9, [rel self]
 call _dprintf
 leave
 ret
