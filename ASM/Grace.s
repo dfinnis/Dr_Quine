097ec49424bbfae69681873678a8670a265bf5f8
Anya Schukin <anya.schukin@gmail.com>
2020-02-06 14:54:06 +0100

    refacto/rm data struct, cleaner

--- ASM/Grace.s
+++ ASM/Grace.s
@@ -3,22 +3,21 @@
 %define DECLARE_MAIN global _main
 %define MAIN _main:
 section .data
-grace:
-.self db SELF, 0
-.kidsname db "Grace_kid.s", 0
+self db SELF, 0
+kidsname db "Grace_kid.s", 0
 section .text
 extern _dprintf
 DECLARE_MAIN
 MAIN
 push rbp
 mov rbp, rsp
-lea rdi, [rel grace.kidsname]
+lea rdi, [rel kidsname]
 mov rsi, 0x0202
 mov rax, 0x2000005
 syscall
 mov rdi, rax
-lea rsi, [rel grace.self]
-lea rdx, [rel grace.self]
+lea rsi, [rel self]
+lea rdx, [rel self]
 mov rcx, 10
 mov r8, 34
 call _dprintf
