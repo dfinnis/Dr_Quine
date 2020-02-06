6b196c5cdbb7246e4c22779c2919e866911d0ace
Anya Schukin <anya.schukin@gmail.com>
2020-02-06 12:49:01 +0100

    feature/colleen self string nearly accurate

--- ASM/Colleen.s
+++ ASM/Colleen.s
@@ -1,6 +1,6 @@
 ; whoami?
 section .data
-self: db "I am Colleen%1$cI can newline%1$c%2$cand also quotes%2$c%1$c"
+self: db "; whoami?%1$csection .data%1$cself: db %2$c%3$s%2$c%1$c%1$csection .text%1$cglobal _main%1$c%1$cextern _printf%1$c%1$c_main:%1$cpush rbp%1$cmov rbp, rsp%1$c%1$c; I must express myself%1$clea rdi, [rel self]%1$cmov rsi, 10%1$cmov rdx, 34%1$clea rcx, [rel self]%1$ccall _printf%1$ccall _return%1$c%1$c_return:%1$cleave%1$cret"
 
 section .text
 global _main
