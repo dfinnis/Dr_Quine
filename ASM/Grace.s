5d3d28d486337fc05107714693bb09edebb14500
Anya Schukin <anya.schukin@gmail.com>
2020-02-06 15:14:43 +0100

    feature/can do newline and quotes

--- ASM/Grace.s
+++ ASM/Grace.s
@@ -1,5 +1,5 @@
 ; I must clone myself, spawning my kid
-%define SELF "here I am"
+%define SELF "oh hi%1$cnewline%1$c%2$cquotes%2$c%3$s"
 %define DECLARE_MAIN global _main
 %define MAIN _main:
 section .data
@@ -17,9 +17,9 @@ mov rax, 0x2000005
 syscall
 mov rdi, rax
 lea rsi, [rel self]
-; lea rdx, [rel self]
-; mov rcx, 10
-; mov r8, 34
+mov rdx, 10
+mov rcx, 34
+lea r8, [rel self]
 call _dprintf
 leave
 ret
