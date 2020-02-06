2c03e1993db66dbfa062b42824d654441a81ffce
Anya Schukin <anya.schukin@gmail.com>
2020-02-06 12:39:31 +0100

    feature/newline and quotes printing correctly from  string

--- ASM/Colleen.s
+++ ASM/Colleen.s
@@ -1,19 +1,23 @@
-;whoami?
+; whoami?
 section .data
-self: db "I am Colleen"
+self: db "I am Colleen%1$cI can newline%1$c%2$cand also quotes%2$c%1$c"
 
 section .text
 global _main
 extern _printf
 
 _main:
-		push rbp
-		mov rbp, rsp
-		;I must express myself
-		lea rdi, [rel self]
-		call _printf
-		call _return
+push rbp
+mov rbp, rsp
+
+; I must express myself
+lea rdi, [rel self]
+mov rsi, 10
+mov rdx, 34
+lea rcx, [rel self]
+call _printf
+call _return
 
 _return:
-		leave
-		ret
+leave
+ret
