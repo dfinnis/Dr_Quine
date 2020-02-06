208a49de089a4d5e31ae7a048c871851b6548bd4
Anya Schukin <anya.schukin@gmail.com>
2020-02-06 20:40:52 +0100

    feature/Sully WIP needs to compile execute and check for _5

--- ASM/test.sh
+++ ASM/test.sh
@@ -37,37 +37,37 @@ diff_5_output=$(eval "$diff_5")
 diff_4="diff Sully.s Sully_4.s"
 diff_4_output=$(eval "$diff_4")
 diff_4_desired="6c6
-< 	int x=5;
+< 	num: dd 5
 ---
-> 	int x=4;"
+> 	num: dd 4"
 
 diff_3="diff Sully.s Sully_3.s"
 diff_3_output=$(eval "$diff_3")
 diff_3_desired="6c6
-< 	int x=5;
+< 	num: dd 5
 ---
-> 	int x=3;"
+> 	num: dd 3"
 
 diff_2="diff Sully.s Sully_2.s"
 diff_2_output=$(eval "$diff_2")
 diff_2_desired="6c6
-< 	int x=5;
+< 	num: dd 5
 ---
-> 	int x=2;"
+> 	num: dd 2"
 
 diff_1="diff Sully.s Sully_1.s"
 diff_1_output=$(eval "$diff_1")
 diff_1_desired="6c6
-< 	int x=5;
+< 	num: dd 5
 ---
-> 	int x=1;"
+> 	num: dd 1"
 
 diff_0="diff Sully.s Sully_0.s"
 diff_0_output=$(eval "$diff_0")
 diff_0_desired="6c6
-< 	int x=5;
+< 	num: dd 5
 ---
-> 	int x=0;"
+> 	num: dd 0"
 
 if [ "$sully_output" == "      14" -a "$diff_5_output" == "" -a "$diff_4_output" == "$diff_4_desired"  -a "$diff_3_output" == "$diff_3_desired"  -a "$diff_2_output" == "$diff_2_desired"  -a "$diff_1_output" == "$diff_1_desired"  -a "$diff_0_output" == "$diff_0_desired" ]
 then
@@ -83,4 +83,5 @@ else
 	echo "$diff_0_output"
 fi
 
-rm Sully Sully.o Sully_*
+rm Sully Sully.o
+#  Sully_*
