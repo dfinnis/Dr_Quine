bf3c916d96347702555e0830d4cdceae60650e0a
Anya Schukin <anya.schukin@gmail.com>
2020-02-07 20:16:13 +0100

    refacto/Colleen most of the way there

--- Python/Colleen.py
+++ Python/Colleen.py
@@ -1,8 +1,8 @@
 # whoami?
 def self():
-    return "I am Colleen %1c"
+    return 'whoami?%cdef self():%c    return %s%cdef main():%c    # I must express myself%c    print(self() % (10, 10, self(), 10, 10, 10, 10, 10))%c%cmain()'
 def main():
     # I must express myself
-    print(self() % (10))
+    print(self() % (10, 10, self(), 10, 10, 10, 10, 10))
 
 main()
