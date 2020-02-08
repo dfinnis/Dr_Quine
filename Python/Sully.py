53b8bfd058f9f10758be0cbf9ef9e3ed7f4529cc
Drew FINNIS <dfinnis@e1r1p14.42.fr>
2020-02-08 12:59:55 +0100

    feature/sully works, but needs to run self and start at 5

--- Python/Sully.py
+++ Python/Sully.py
@@ -4,9 +4,9 @@ def sully():
         if x <= 0:
             return
         filename="Sully_"+str(x-1)+".py"
-        selfevolved='def sully():%c try:%c      x=%d%c     if x <= 0:%c            return%c        filename="Sully_"+str(x-1)+".py"%c      selfevolved=%r%c        with open(filename, "w") as child:%c            child.write(selfevolved%%(10, x-1, 10, 10, 10, 10, selfevolved, 10, 10, 10, 10, 10, 10, 10, 10))%c  except:%c       return%c%csully()%c'
+        selfevolved='def sully():%c    try:%c        x=%d%c        if x <= 0:%c            return%c        filename="Sully_"+str(x-1)+".py"%c        selfevolved=%r%c        with open(filename, "w") as child:%c            child.write(selfevolved %% (10,10,x-1,10,10,10,10,selfevolved,10,10,10,10,10,10,10))%c    except:%c        return%c%csully()%c'
         with open(filename, "w") as child:
-            child.write(selfevolved%(10, 10, x-1, 10, 10, 10, 10, selfevolved, 10, 10, 10, 10, 10, 10, 10))
+            child.write(selfevolved % (10,10,x-1,10,10,10,10,selfevolved,10,10,10,10,10,10,10))
     except:
         return
 
