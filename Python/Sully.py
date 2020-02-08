d8123f5bdb43b40c42e02966fb0754ee33de0d50
Anya Schukin <anya.schukin@gmail.com>
2020-02-08 19:13:08 +0100

    fix/sully starts at 5 and ends at 0

--- Python/Sully.py
+++ Python/Sully.py
@@ -1,14 +1,15 @@
 import os
+import sys
 
 def sully():
     try:
-        # decrement x if sully_5.py exists
         x=5
+        if sys.argv[0] == "Sully.py": x+=1
         filename="Sully_"+str(x-1)+".py"
-        selfevolved='import os%c%cdef sully():%c    try:%c        x=%d%c        filename="Sully_"+str(x-1)+".py"%c        selfevolved=%r%c        with open(filename, "w") as child:%c            child.write(selfevolved %% (10,10,10,10,x-1,10,10,selfevolved,10,10,10,10,10,10,10,10,10,10))%c        if x <= 0:%c            return%c        os.system(\'python3 \' + filename)%c    except:%c        return%c%csully()%c'
+        selfevolved='import os%cimport sys%c%cdef sully():%c    try:%c        x=%d%c        if sys.argv[0] == "Sully.py": x+=1%c        filename="Sully_"+str(x-1)+".py"%c        selfevolved=%r%c        with open(filename, "w") as child:%c            child.write(selfevolved %% (10,10,10,10,10,x-1,10,10,10,selfevolved,10,10,10,10,10,10,10,10,10,10))%c        if x < 2:%c            return%c        os.system(\'python3 \' + filename)%c    except:%c        return%c%csully()%c'
         with open(filename, "w") as child:
-            child.write(selfevolved % (10,10,10,10,x-1,10,10,selfevolved,10,10,10,10,10,10,10,10,10,10))
-        if x <= 0:
+            child.write(selfevolved % (10,10,10,10,10,x-1,10,10,10,selfevolved,10,10,10,10,10,10,10,10,10,10))
+        if x < 2:
             return
         os.system('python3 ' + filename)
     except:
