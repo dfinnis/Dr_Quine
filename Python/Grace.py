5e9259a96de9051ff1c47321703f5871f1fbe3a6
Drew FINNIS <dfinnis@e1r1p14.42.fr>
2020-02-08 11:05:03 +0100

    refacto/Grace shorter

--- Python/Grace.py
+++ Python/Grace.py
@@ -1,9 +1,9 @@
 # I must clone myself, spawning my kid
-SELF = '# I must clone myself, spawning my kid%cSELF = %r%cKID = %r%cCLONE = \'\'\'try:%c    with open(KID, "w") as kid:%c        kid.write(SELF %% (10, SELF, 10, KID, 10, 10, 10, 10, 10, 10, 10))%cexcept:%c    pass\'\'\'%c%cexec(CLONE)'
+SELF = '# I must clone myself, spawning my kid%cSELF = %r%cKID = %r%cCLONE = \'\'\'try:%c    with open(KID, "w") as kid:%c        kid.write(SELF %% (10,SELF,10,KID,10,10,10,10,10,10,10))%cexcept:%c    pass\'\'\'%c%cexec(CLONE)'
 KID = 'Grace_kid.py'
 CLONE = '''try:
     with open(KID, "w") as kid:
-        kid.write(SELF % (10, SELF, 10, KID, 10, 10, 10, 10, 10, 10, 10))
+        kid.write(SELF % (10,SELF,10,KID,10,10,10,10,10,10,10))
 except:
     pass'''
 
