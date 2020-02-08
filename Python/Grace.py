2f809ff5c6a44493ebf3b405a45920a9a6c13434
Drew FINNIS <dfinnis@e1r1p14.42.fr>
2020-02-08 10:15:19 +0100

    feature/Grace writes to kid, need to build self string

--- Python/Grace.py
+++ Python/Grace.py
@@ -1,8 +1,10 @@
-# whoami?
-def self():
-    return "I am Grace %1c"
-def main():
-    # I must express myself
-    print(self() % (10))
+# I must clone myself, spawning my kid
+SELF = 'Grace%c%r%c%r%cend'
+KID = 'Grace_kid.py'
+CLONE = '''try:
+    with open(KID, "w") as kid:
+        kid.write(SELF % (10, KID, 10, SELF, 10))
+except:
+    pass'''
 
-main()
+exec(CLONE)
