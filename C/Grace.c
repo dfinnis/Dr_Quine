66549abd4fcec224870fa1b2cc0d74d03ac9f2c6
Anya Schukin <anya.schukin@gmail.com>
2020-02-08 21:34:35 +0100

    refacto/c grace names clearer

--- C/Grace.c
+++ C/Grace.c
@@ -3,7 +3,7 @@
 /*
    I must define and clone myself, spawning my kid
 */
-#define SELF		"#include <stdio.h>%1$c#include <unistd.h>%1$c/*%1$c   I must define and clone myself, spawning my kid%1$c*/%1$c#define SELF		%2$c%3$s%2$c%1$c#define PRINT(f)	fprintf(f,SELF,10,34,SELF)%1$c#define FT 			int main() { FILE *f; f=fopen(%2$cGrace_kid.c%2$c,%2$cw%2$c); PRINT(f); return 0; }%1$cFT"
-#define PRINT(f)	fprintf(f,SELF,10,34,SELF)
-#define FT 			int main() { FILE *f; f=fopen("Grace_kid.c","w"); PRINT(f); return 0; }
+#define SELF			"#include <stdio.h>%1$c#include <unistd.h>%1$c/*%1$c   I must define and clone myself, spawning my kid%1$c*/%1$c#define SELF			%2$c%3$s%2$c%1$c#define CLONE(kid)	fprintf(kid,SELF,10,34,SELF)%1$c#define FT 				int main() { FILE *kid; kid=fopen(%2$cGrace_kid.c%2$c,%2$cw%2$c); CLONE(kid); return 0; }%1$cFT"
+#define CLONE(kid)	fprintf(kid,SELF,10,34,SELF)
+#define FT 				int main() { FILE *kid; kid=fopen("Grace_kid.c","w"); CLONE(kid); return 0; }
 FT
