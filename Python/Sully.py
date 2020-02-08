8db94a05c8195d38dba313464a1fb84985d4c8c2
Drew FINNIS <dfinnis@e1r1p14.42.fr>
2020-02-08 11:43:54 +0100

    refacto/sully need to fix whiespaces

--- Python/Sully.py
+++ Python/Sully.py
@@ -1,8 +1,13 @@
-# whoami?
-def self():
-    return "I am Sully %1c"
-def main():
-    # I must express myself
-    print(self() % (10))
+def sully():
+    try:
+        x=5
+        if x <= 0:
+            return
+        filename="Sully_"+str(x-1)+".py"
+        selfevolved='def sully():%c try:%c      x=%d%c     if x <= 0:%c            return%c        filename="Sully_"+str(x-1)+".py"%c      selfevolved=%r%c        with open(filename, "w") as child:%c            child.write(selfevolved%%(10, x-1, 10, 10, 10, 10, selfevolved, 10, 10, 10, 10, 10, 10, 10, 10))%c  except:%c       return%c%csully()%c'
+        with open(filename, "w") as child:
+            child.write(selfevolved%(10, 10, x-1, 10, 10, 10, 10, selfevolved, 10, 10, 10, 10, 10, 10, 10))
+    except:
+        return
 
-main()
+sully()
