8d00fbdeeca9375356d5bfef4559d4086ad34f2e
Drew FINNIS <dfinnis@e1r1p14.42.fr>
2020-02-08 11:01:14 +0100

    feature/Grace correct

--- Python/Grace.py
+++ Python/Grace.py
@@ -1,9 +1,9 @@
 # I must clone myself, spawning my kid
-SELF = 'Grace%c%r%c%r%cend'
+SELF = '# I must clone myself, spawning my kid%cSELF = %r%cKID = %r%cCLONE = \'\'\'try:%c    with open(KID, "w") as kid:%c        kid.write(SELF %% (10, SELF, 10, KID, 10, 10, 10, 10, 10, 10, 10))%cexcept:%c    pass\'\'\'%c%cexec(CLONE)'
 KID = 'Grace_kid.py'
 CLONE = '''try:
     with open(KID, "w") as kid:
-        kid.write(SELF % (10, KID, 10, SELF, 10))
+        kid.write(SELF % (10, SELF, 10, KID, 10, 10, 10, 10, 10, 10, 10))
 except:
     pass'''
 
