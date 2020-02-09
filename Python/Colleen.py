0d47d6ccc0ea5cc2073752ae314684d653e38190
Drew FINNIS <dfinnis@e2r1p10.42.fr>
2020-02-09 13:44:13 +0100

    feature/ASM makefile

--- Python/Colleen.py
+++ Python/Colleen.py
@@ -1,8 +1,8 @@
 # whoami?
 def self():
-    return '# whoami?%cdef self():%c    return %r%cdef main():%c    # I must express myself%c    print(self() %% (10, 10, self(), 10, 10, 10, 10, 10))%c%cmain()'
+    return '# whoami?%cdef self():%c    return %r%cdef main():%c    # I must express myself%c    print(self() %% (10,10,self(),10,10,10,10,10))%c%cmain()'
 def main():
     # I must express myself
-    print(self() % (10, 10, self(), 10, 10, 10, 10, 10))
+    print(self() % (10,10,self(),10,10,10,10,10))
 
 main()
