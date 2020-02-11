a07cc3a3529562e46da9287f3ed7079ca1f9de0f
Drew FINNIS <dfinnis@e1r1p14.42.fr>
2020-02-11 08:19:07 +0100

    fix/c grace fopen to open, check fd, avoid segfauilt

--- C/Grace.c
+++ C/Grace.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
-#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
 /*
    I must define and clone myself, spawning my kid
 */
-#define SELF			"#include <stdio.h>%1$c#include <unistd.h>%1$c/*%1$c   I must define and clone myself, spawning my kid%1$c*/%1$c#define SELF			%2$c%3$s%2$c%1$c#define CLONE(kid)	fprintf(kid,SELF,10,34,SELF)%1$c#define FT 				int main() { FILE *kid; kid=fopen(%2$cGrace_kid.c%2$c,%2$cw%2$c); CLONE(kid); return 0; }%1$cFT"
-#define CLONE(kid)	fprintf(kid,SELF,10,34,SELF)
-#define FT 				int main() { FILE *kid; kid=fopen("Grace_kid.c","w"); CLONE(kid); return 0; }
+#define SELF		"#include <stdio.h>%1$c#include <stdlib.h>%1$c#include <fcntl.h>%1$c/*%1$c   I must define and clone myself, spawning my kid%1$c*/%1$c#define SELF		%2$c%3$s%2$c%1$c#define CLONE(kid)	dprintf(kid,SELF,10,34,SELF)%1$c#define FT 			int main() { int kid = open(%2$cGrace_kid.c%2$c, O_WRONLY | O_CREAT | O_TRUNC, 0644); if (kid < 0) { exit(1); }; CLONE(kid); return 0; }%1$cFT"
+#define CLONE(kid)	dprintf(kid,SELF,10,34,SELF)
+#define FT 			int main() { int kid = open("Grace_kid.c", O_WRONLY | O_CREAT | O_TRUNC, 0644); if (kid < 0) { exit(1); }; CLONE(kid); return 0; }
 FT
