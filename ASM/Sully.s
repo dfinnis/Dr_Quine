e70baf6807a4d293a6840e80e6aea1b281e03b48
Drew FINNIS <dfinnis@e1r1p14.42.fr>
2020-02-12 10:59:19 +0100

    fix/check filename fixed, now checks for - not sully.s

--- ASM/Sully.s
+++ ASM/Sully.s
@@ -1,9 +1,9 @@
 section .data
-self: db "section .data%1$cself: db %2$c%3$s%2$c,0%1$cfilename: db %2$cSully_%%d.s%2$c,0%1$ccompile: db %2$cnasm -f macho64 Sully_%%1$d.s; gcc Sully_%%1$d.o -o Sully_%%1$d%2$c,0%1$cexecute: db %2$c./Sully_%%1$d%2$c,0%1$cinitial: db %2$cSully.s%2$c,0%1$cfilename_str: times 100 db 0%1$cexecute_str: times 100 db 0%1$ccompile_str: times 100 db 0%1$cMY_FILENAME db __FILE__, 0%1$c%1$csection .text%1$cglobal _start%1$cglobal _main%1$cextern _system%1$cextern _dprintf%1$cextern _sprintf%1$cextern _strcmp%1$c%1$c_start:%1$c_main:%1$c	push rbp%1$c	mov rbp, rsp%1$c%1$c	; Establish x%1$c	mov r12, %4$d%1$c	lea rdi, [rel initial]%1$c	lea rsi, [rel MY_FILENAME]%1$c	call _strcmp%1$c	test rax, rax%1$c	jz .dec%1$c	dec r12%1$c%1$c.dec:%1$c	; Create filename%1$c	lea rdi, [rel filename_str]%1$c	lea rsi, [rel filename]%1$c	mov rdx, r12%1$c	call _sprintf%1$c%1$c	; Open file%1$c	mov rax, 0x2000005		; Open file syscall%1$c	lea rdi, [rel filename_str]%1$c	mov rsi, 03001o			; Create, write only%1$c	mov rdx, 0644o			; Permissions%1$c	syscall%1$c	cmp rax, 0%1$c	jb .ret%1$c%1$c	; Print self in file%1$c	mov rdi, rax%1$c	lea rsi, [rel self]%1$c	mov rdx, 10%1$c	mov rcx, 34%1$c	lea r8, [rel self]%1$c	mov r9, r12%1$c	call _dprintf%1$c%1$c	; Close file%1$c	mov rax, 0x20000006		; Close file syscall%1$c	syscall%1$c%1$c	; Compile%1$c	lea rdi, [rel compile_str]%1$c	lea rsi, [rel compile]%1$c	mov rdx, r12%1$c	call _sprintf%1$c	lea rdi, [rel compile_str]%1$c	call _system%1$c%1$c	; Stop at Sully_0%1$c	cmp r12, 0%1$c	jle .ret%1$c%1$c	; Execute%1$c	lea rdi, [rel execute_str]%1$c	lea rsi, [rel execute]%1$c	mov rdx, r12%1$c	call _sprintf%1$c	lea rdi, [rel execute_str]%1$c	call _system%1$c%1$c.ret:%1$c	leave%1$c	ret%1$c",0
+self: db "section .data%1$cself: db %2$c%3$s%2$c,0%1$cfilename: db %2$cSully_%%d.s%2$c,0%1$ccompile: db %2$cnasm -f macho64 Sully_%%1$d.s; gcc Sully_%%1$d.o -o Sully_%%1$d%2$c,0%1$cexecute: db %2$c./Sully_%%1$d%2$c,0%1$cinitial_str: times 100 db 0%1$cfilename_str: times 100 db 0%1$cexecute_str: times 100 db 0%1$ccompile_str: times 100 db 0%1$cMY_FILENAME db __FILE__, 0%1$c%1$csection .text%1$cglobal _start%1$cglobal _main%1$cextern _system%1$cextern _dprintf%1$cextern _sprintf%1$cextern _strcmp%1$c%1$c_start:%1$c_main:%1$c	push rbp%1$c	mov rbp, rsp%1$c%1$c	; Establish x%1$c	mov r12, %4$d%1$c%1$c	lea rdi, [rel initial_str]%1$c	lea rsi, [rel filename]%1$c	mov rdx, r12%1$c	call _sprintf%1$c%1$c	lea rdi, [rel initial_str]%1$c	lea rsi, [rel MY_FILENAME]%1$c	call _strcmp%1$c	test rax, rax%1$c	jnz .dec%1$c	dec r12%1$c%1$c.dec:%1$c	; Create filename%1$c	lea rdi, [rel filename_str]%1$c	lea rsi, [rel filename]%1$c	mov rdx, r12%1$c	call _sprintf%1$c%1$c	; Open file%1$c	mov rax, 0x2000005		; Open file syscall%1$c	lea rdi, [rel filename_str]%1$c	mov rsi, 03001o			; Create, write only%1$c	mov rdx, 0644o			; Permissions%1$c	syscall%1$c	cmp rax, 0%1$c	jb .ret%1$c%1$c	; Print self in file%1$c	mov rdi, rax%1$c	lea rsi, [rel self]%1$c	mov rdx, 10%1$c	mov rcx, 34%1$c	lea r8, [rel self]%1$c	mov r9, r12%1$c	call _dprintf%1$c%1$c	; Close file%1$c	mov rax, 0x20000006		; Close file syscall%1$c	syscall%1$c%1$c	; Compile%1$c	lea rdi, [rel compile_str]%1$c	lea rsi, [rel compile]%1$c	mov rdx, r12%1$c	call _sprintf%1$c	lea rdi, [rel compile_str]%1$c	call _system%1$c%1$c	; Stop at Sully_0%1$c	cmp r12, 0%1$c	jle .ret%1$c%1$c	; Execute%1$c	lea rdi, [rel execute_str]%1$c	lea rsi, [rel execute]%1$c	mov rdx, r12%1$c	call _sprintf%1$c	lea rdi, [rel execute_str]%1$c	call _system%1$c%1$c.ret:%1$c	leave%1$c	ret%1$c",0
 filename: db "Sully_%d.s",0
 compile: db "nasm -f macho64 Sully_%1$d.s; gcc Sully_%1$d.o -o Sully_%1$d",0
 execute: db "./Sully_%1$d",0
-initial: db "Sully.s",0
+initial_str: times 100 db 0
 filename_str: times 100 db 0
 execute_str: times 100 db 0
 compile_str: times 100 db 0
@@ -24,11 +24,17 @@ _main:
 
 	; Establish x
 	mov r12, 5
-	lea rdi, [rel initial]
+
+	lea rdi, [rel initial_str]
+	lea rsi, [rel filename]
+	mov rdx, r12
+	call _sprintf
+
+	lea rdi, [rel initial_str]
 	lea rsi, [rel MY_FILENAME]
 	call _strcmp
 	test rax, rax
-	jz .dec
+	jnz .dec
 	dec r12
 
 .dec:
